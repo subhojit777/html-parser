0ee796d8b592b0aceefa0f7d267524e199bff215
Subhojit Paul <subhojitpaul21@gmail.com>
2017-02-05 18:56:21 +0530

    Html parser init

--- src/view.jsx
+++ src/view.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import HtmlParser from 'htmlparser2'
 
 class View extends Component {
   constructor(props) {
     super(props)
+
+    this.parseHtml = this.parseHtml.bind(this)
+  }
+
+  parseHtml() {
+    let handler = new HtmlParser.DomHandler((err, dom) => {
+      if (err)
+        // @TODO change this.
+        console.log(err)
+      else
+        // Use this to create the view.
+        console.log(dom)
+    })
+
+    let parser = new HtmlParser.Parser(handler)
+    parser.write(this.props.markup)
+    parser.done()
   }
 
   render() {
     return (
-      <div>{ this.props.markup }</div>
+      <div>{ this.parseHtml() }</div>
     )
   }
 }
